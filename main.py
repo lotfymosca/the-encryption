ef359d934ce753003d19d596b52b8ae33eef6abb
Lotfymosca <92894898+lotfymosca@users.noreply.github.com>
2022-12-01 12:50:04 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,84 @@
+print("mosca enrypptor")
+print ("mosca is the best programmer")
+code = input("enter the c0ode: ")
+num1 = ("a")
+num2 = ("b")
+num3 = ("c")
+num4 = ("d")
+num5 = ("e")
+num6 = ("f")
+num7 = ("g")
+num8 = ("h")
+num9 = ("i")
+num10 = ("j")
+num11 = ("k")
+num12 = ("l")
+num13 = ("m")
+num14 = ("n")
+num15 = ("o")
+num16 = ("p")
+num17 = ("q")
+num18 = ("r")
+num19 = ("s")
+num20= ("t")
+num21 = ("u")
+num22 = ("v")
+num23 = ("w")
+num24= ("x")
+num25 = ("y")
+num26 = ("z")
+
+if code == '1603' :
+ print(num1)
+if code == '1603':
+ print(num2)
+if code == '1603' :
+ print(num3)
+if code == '1603' :
+ print(num4)
+if code == '1603' :
+ print(num5)
+if code == '1603' :
+ print(num6)
+if code == '1603' :
+ print(num7)
+if code == '1603' :
+ print(num8)
+if code == '1603' :
+ print(num9)
+if code == '1603' :
+ print(num10)
+if code == '1603' :
+ print(num11)
+if code == '1603' :
+ print(num12)
+if code == '1603' :
+ print(num13)
+if code == '1603' :
+ print(num13)
+if code == '1603' :
+ print(num14)
+if code == '1603' :
+ print(num15)
+if code == '1603' :
+ print(num16)
+if code == '1603' :
+ print(num17)
+if code == '1603' :
+ print(num18)
+if code == '1603' :
+ print(num19)
+if code == '1603' :
+ print(num20)
+if code == '1603' :
+ print(num21)
+if code == '1603' :
+ print(num22)
+if code == '1603' :
+ print(num23)
+if code == '1603' :
+ print(num24)
+if code == '1603' :
+ print(num25)
+if code == '1603' :
+ print(num26)
